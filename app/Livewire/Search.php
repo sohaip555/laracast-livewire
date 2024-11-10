472d7d4df259edd150708e1cf213b7f82793a980
Sohaip Abughrara <sohupabughrara@gmail.com>
2024-11-10 11:55:43 +0200

    add Url prorperty to sreach component

--- app/Livewire/Search.php
+++ app/Livewire/Search.php
@@ -5,38 +5,29 @@ namespace App\Livewire;
 use App\Models\Article;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Reactive;
+use Livewire\Attributes\Url;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Search extends Component
 {
-    #[Validate('required')]
+
+    #[Url(as: 'q', except: '', history: true)]
     public $searchText = '';
+    public $placeHolder = "type something to search";
 
-    public $results = [];
-
-
-    public function updatedSearchText($value)
-    {
-        $this->reset('results');
-        $this->validate();
-        $searchTeem = "%{$value}%";
-
-        $this->results = Article::where('title', 'like',$searchTeem)->get();
-//        dd($this->results);
-    }
 
     #[On('search:clear-results')]
     public function clear()
     {
-        $this->reset('results', 'searchText');
-//        $this->validate();
-
+        $this->reset( 'searchText');
     }
 
     public function render()
     {
-        return view('livewire.search');
+        return view('livewire.search',[
+            'results' => Article::where('title', 'like', "%{$this->searchText}%")->get(),
+        ]);
     }
 
 
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -9,12 +9,12 @@
                     type="text"
                     class="px-2 w-60 border rounded-md bg-gray-700 text-white"
                     wire:model.live.debounce="searchText"
-                    placeholder="type something to search"
+                    placeholder= {{$placeHolder}}
                 >
 
-{{--                <button class="p-4 text-white font-medium disabled:bg-indigo-400 rounded-md bg-indigo-600"--}}
-{{--                wire:click.prevent="clear()"--}}
-{{--                {{empty($searchText) ? 'disabled' : ''}}>Clear</button>--}}
+                <button class="p-4 text-white font-medium disabled:bg-indigo-400 rounded-md bg-indigo-600"
+                wire:click.prevent="clear()"
+                {{empty($searchText) ? 'disabled' : ''}}>Clear</button>
             </div>
 
         </form>
